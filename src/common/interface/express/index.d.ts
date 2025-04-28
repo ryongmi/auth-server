9b29570bc245a00f6edcfb34ddc4cd4aff31be7d
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-04-28 17:22:18 +0900

    jwt 가드 생성
    추후 필요한 에세스 토큰 및 리프레쉬 토큰 존재 유무 및 유효한 토큰인지 검증하는 가드 추가
    - request에 복호화한 유저정보를 넣어야해서 우선 user 속성 추가 -> 추후 변경가능성 있음

---
+++ src/common/interface/express/index.d.ts
@@ -0,0 +1,7 @@
+import { UserPayload } from '../user-payload.interface'; // 토큰 payload 타입
+
+declare module 'express' {
+  export interface Request {
+    user?: UserPayload; // 여기에 원하는 타입 지정
+  }
+}
---
+++ src/common/interface/user-payload.interface.ts
@@ -0,0 +1,5 @@
+export interface UserPayload {
+  userId: number;
+  email: string;
+  // 필요하면 다른 필드 추가
+}
