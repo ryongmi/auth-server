9c11a21b934c3c843bcb5b113959217c2746c90a
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-05-05 02:44:11 +0900

    jwt exception 추가
    - jwt 관련 에러 관리 파일 추가

---
+++ src/exception/jwt.exception.ts
@@ -0,0 +1,15 @@
+import {
+  BadRequestException,
+  ForbiddenException,
+  HttpException,
+  HttpStatus,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+export class JwtException {
+  static refreshTokenNotFound(): HttpException {
+    return new UnauthorizedException('No refresh token provided');
+  }
+}
