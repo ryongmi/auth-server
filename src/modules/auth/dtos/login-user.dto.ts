a0f03c52d0df885024da1d9761b97c3ed653d996
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-05-05 23:14:47 +0900

    중복 파일 삭제

--- src/modules/auth/dtos/login-user.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PickType } from '@nestjs/swagger';
-import { User } from '../../user/entities';
-
-export class LoginUserDto extends PickType(User, [
-  'email',
-  'password',
-] as const) {}
