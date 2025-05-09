e65a11f8e3fbd0e70609e45781ca72a31cb5742c
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-05-09 15:33:03 +0900

    repository 공통 함수 수정
    - id조건 조회 함수에 릴레이션 추가한 함수 추가
    - 전체조회 함수 제거
       -> 보통 필터를 사용할 예정이라 공통으로 넣는것보다 각각의 리포지토리에 작성하는게 맞다고 판단

--- src/common/repositories/base.repository.ts
+++ src/common/repositories/base.repository.ts
@@ -6,7 +6,6 @@ import {
 } from 'typeorm';
 import { NotFoundException } from '@nestjs/common';
 import { DataSource } from 'typeorm';
-import { UserQueryDto } from 'src/modules/user/dtos';
 
 export class BaseRepository<T> extends Repository<T> {
   constructor(entity: EntityTarget<T>, dataSource: DataSource) {
@@ -21,6 +20,20 @@ export class BaseRepository<T> extends Repository<T> {
   async findOneById(id: number | string): Promise<T | undefined> {
     const where = { id } as unknown as FindOptionsWhere<T>;
     return this.findOne({ where });
+  }
+
+  /**
+   * ID로 엔티티 하나를 조회합니다.
+   * @param id 엔티티 ID
+   * @param relations 함께 조회할 테이블 이름 배열
+   * @returns 찾은 엔티티
+   */
+  async findOneByIdWithRelations(
+    id: number | string,
+    relations: Array<string>,
+  ): Promise<T | undefined> {
+    const where = { id } as unknown as FindOptionsWhere<T>;
+    return this.findOne({ where, relations });
   }
 
   /**
@@ -39,31 +52,6 @@ export class BaseRepository<T> extends Repository<T> {
     }
 
     return entity;
-  }
-
-  /**
-   * 모든 엔티티를 조회합니다.
-   * @returns 모든 엔티티 배열
-   */
-  async findAll(
-    query: UserQueryDto,
-    alias: string,
-  ): Promise<{ items: T[]; total: number; page: number; limit: number }> {
-    const { page, limit, sortOrder, sortBy } = query;
-    const skip = (page - 1) * limit;
-
-    const [items, total] = await this.createQueryBuilder(alias)
-      .orderBy(`${alias}.${sortBy}`, sortOrder)
-      .skip(skip)
-      .take(limit)
-      .getManyAndCount();
-
-    return {
-      items,
-      total,
-      page,
-      limit,
-    };
   }
 
   // async findAll(): Promise<T[]> {
