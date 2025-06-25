fc1acdc63c7420935f16f8f8c538046dded2177c
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-25 12:37:45 +0900

    feat(oauth) - Index 및 Unique 추가

--- src/modules/oauth/entities/oauth-account.entity.ts
+++ src/modules/oauth/entities/oauth-account.entity.ts
@@ -1,20 +1,22 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, Index, Unique } from 'typeorm';
 
 import { BaseEntityUUID } from '@krgeobuk/core/entities';
 import { ProviderType } from '@krgeobuk/oauth/enum';
 
 @Entity('oauth_account')
-// @Unique(['id', 'userId'])
+@Index(['id', 'userId'], { unique: true })
+@Unique(['userId', 'provider'])
 export class OAuthAccount extends BaseEntityUUID {
   @Column({ type: 'varchar', length: 255 })
   providerId!: string; // OAuth 제공자 고유 ID
 
-  @Column({ type: 'enum', enum: ProviderType, unique: true })
+  @Column({ type: 'enum', enum: ProviderType })
   provider!: ProviderType; // google, naver 등
 
   // @Column({ type: 'varchar', length: 255 })
   // email!: string; // OAuth에서 받은 이메일, 통합 판단 시 참고용
 
-  @Column({ type: 'uuid', unique: true })
+  @Index('IDX_USER_ID')
+  @Column({ type: 'uuid' })
   userId!: string; // FK 없이 userId 저장해서 직접 조회
 }
