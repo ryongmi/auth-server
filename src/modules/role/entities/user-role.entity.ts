fe5a356118bfd7c0db28bfd697f29b4aa145169a
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-30 09:54:02 +0900

    feat(user-role) - index 추가

--- src/modules/role/entities/user-role.entity.ts
+++ src/modules/role/entities/user-role.entity.ts
@@ -1,6 +1,8 @@
-import { Entity, PrimaryColumn } from 'typeorm';
+import { Entity, Index, PrimaryColumn } from 'typeorm';
 
 @Entity('user_role')
+@Index('IDX_USER_ROLE_USER', ['userId']) // ✅ 사용자 기준 인덱스
+@Index('IDX_USER_ROLE_ROLE', ['roleId']) // ✅ 역할 기준 인덱스
 export class UserRole {
   @PrimaryColumn({ type: 'uuid' })
   userId!: string;
