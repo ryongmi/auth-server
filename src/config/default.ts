7534c37a449c55f41f2147ac8598ba851ff26905
ryongmi <62281754+ryongmi@users.noreply.github.com>
2025-06-30 09:52:07 +0900

    chore(config) - 주석제거

--- src/config/default.ts
+++ src/config/default.ts
@@ -1,9 +1,19 @@
-export default (): {
-  mode: string | undefined;
-  port: number;
-  corsOrigins: string | undefined;
-} => ({
-  mode: process.env.NODE_ENV,
-  port: parseInt(process.env.PORT ?? '8000', 10),
-  corsOrigins: process.env.CORS_ORIGINS,
-});
+import { DefaultConfig } from '@common/interfaces/index.js';
+
+export default (): DefaultConfig => {
+  const mode = process.env.NODE_ENV;
+
+  if (mode !== 'local' && mode !== 'development' && mode !== 'production') {
+    return {
+      mode: undefined,
+      port: parseInt(process.env.PORT ?? '8000', 10),
+      corsOrigins: process.env.CORS_ORIGINS,
+    };
+  }
+
+  return {
+    mode,
+    port: parseInt(process.env.PORT ?? '8000', 10),
+    corsOrigins: process.env.CORS_ORIGINS,
+  };
+};
